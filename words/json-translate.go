8d4558f0f7dcfbad26fbd8f8a8cd119673c2beb2
kidinamoto01 <kidinamoto@qq.com>
2018-01-13 13:36:29 +0800

    add http json demo

---
+++ words/json-translate.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Server struct {
+	ServerName string
+	ServerIP   string
+}
+
+type Serverslice struct {
+	Servers []Server
+}
+
+func main() {
+	var s Serverslice
+	str := `{"servers":[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
+	json.Unmarshal([]byte(str), &s)
+	fmt.Println(s)
+
+	var s2 Serverslice
+	s2.Servers = append(s.Servers, Server{ServerName: "Shanghai_VPN", ServerIP: "127.0.0.1"})
+	s2.Servers = append(s.Servers, Server{ServerName: "Beijing_VPN", ServerIP: "127.0.0.2"})
+	b, err := json.Marshal(s)
+	if err != nil {
+		fmt.Println("json err:", err)
+	}
+	fmt.Println(string(b))
+}
